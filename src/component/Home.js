9e7cebe73cf9292797a19df540006919b3b86b90
rizkianakbar <rizkianakbar4@gmail.com>
2019-12-09 23:31:38 +0700

    add jumbotron

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { Jumbotron, Container } from "react-bootstrap";
 import "../App.css";
-
+import jcover1 from "../assets/jcover1.png";
+import jcover2 from "../assets/jcover2.png";
 export default function Home() {
   return (
     <div>
       <Jumbotron fluid className="jumbotron">
         <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
+          <img
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            src={jcover1}
+            alt="cover jumbotron 1"
+          />
+          <h1
+            className="text-center"
+            style={{ color: "white", fontSize: "100px" }}
+          >
+            Belserion Hotel
+          </h1>
+          <h1
+            className="text-center"
+            style={{ color: "white", fontSize: "50px" }}
+          >
+            Luxury Resort & Spa
+          </h1>
+          <img
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            src={jcover2}
+            alt="cover jumbotron 1"
+          />
         </Container>
       </Jumbotron>
     </div>
