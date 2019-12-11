4b6d2c53a0c98bcfc5aec28bf1ee33768d6f782d
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-11 13:07:43 +0700

    Change value of route exact to boolean

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact="true">
+        <Route path="/" exact={true}>
           <Home />
         </Route>
         <Route path="/about">
