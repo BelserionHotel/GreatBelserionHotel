da50980c36702174e704f1bdabe8be22ed99bddd
rizkianakbar <rizkianakbar4@gmail.com>
2019-12-16 03:37:34 +0700

    delete file

--- src/Pages/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Gallery } from "./Gallery";
-export { default as About } from "./About";
-export { default as Services } from "./Services";
-export { default as Contact } from "./Contact";
-export { default as News } from "./News";
-export { default as Rooms } from "./Rooms";
-export { default as Signin } from "./Signin";
-export { default as Signup } from "./Signup";
