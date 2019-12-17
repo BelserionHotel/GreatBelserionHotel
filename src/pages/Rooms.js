99815b4fa1177c383bab2f5b500dfcd2becf7ce0
rizkianakbar <rizkianakbar4@gmail.com>
2019-12-17 14:57:26 +0700

    fetch data rooms from api

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,4 +1,3 @@
-import React, { Fragment } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import "../App.css";
 // import jcover1 from "../assets/jcover1.png";
@@ -6,205 +5,160 @@ import jcover2 from "../assets/jcover2.png";
 import { HeaderAll } from "../component";
 
 import RoomCard from "../component/RoomCard";
+import React, { Component, Fragment } from "react";
 
-export default function Rooms() {
-  const roomData = [
-    {
-      name: "Standard Single Room",
-      description:
-        "Standard Single Rooms are designed in open-concept living area and have many facilities",
-      info: {
-        adults: 2,
-        view: "swimming pool",
-        size: 30,
-        bedType: "queen bed",
-        categories: "single",
-        amenities:
-          "air-conditioning, free wi-fi, hairdryer, in-room safety,laundry, minibar, telephone"
-      }
-    },
-    {
-      name: "Classic Double Room",
-      description:
-        "Classic Double Rooms are perfectly equipped for traveling couples or friends.",
-      info: {
-        adults: 4,
-        view: "swimming pool",
-        size: 50,
-        bedType: "queen bed",
-        categories: "double",
-        amenities:
-          "air-conditioning, free wi-fi, hairdryer, in-room safety,laundry, minibar, telephone"
-      }
-    },
-    {
-      name: "Superior Double Room",
-      description:
-        "Have lots of in-room facilities and are designed in open-concept living area.",
-      info: {
-        adults: 4,
-        view: "beach",
-        size: 50,
-        bedType: "queen bed",
-        categories: "double",
-        amenities:
-          "air-conditioning, free wi-fi, hairdryer, in-room safety, laundry, minibar, telephone"
-      }
-    },
-    {
-      name: "Comfort Triple Room",
-      description:
-        "Perfectly suitable for family vacation or for big companies.",
-      info: {
-        adults: 6,
-        view: "beach",
-        size: 50,
-        bedType: "queen beach",
-        categories: "double",
-        amenities:
-          "air-conditioning, free wi-fi, hairdryer, in-room safety,laundry, minibar, telephone"
-      }
-    }
-  ];
+export default class Rooms extends Component {
+  state = {
+    data: []
+  };
 
-  var roomCards = roomData.map(data => {
-    return <RoomCard roomData={data} />;
-  });
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          data: json
+        });
+      });
+  }
 
-  return (
-    <Fragment>
-      <HeaderAll jumbotronTitle="Rooms" />
+  render() {
+    return (
+      <Fragment>
+        <HeaderAll jumbotronTitle="Rooms" />
 
-      <div className="about1">
-        <br />
-        <br />
-        <br />
-        <Container style={{ backgroundColor: "white" }}>
-          <Row
-            style={{
-              height: "120px",
-              boxShadow: "0 13px 43px 0 rgba(37, 46, 89, 0.1)"
-            }}
-          >
-            <Col
-              lg={3}
-              xs={12}
-              md={6}
+        <div className="about1">
+          <br />
+          <br />
+          <br />
+          <Container style={{ backgroundColor: "white" }}>
+            <Row
               style={{
-                backgroundColor: "white",
-                marginTop: "20px",
-                color: "black"
+                height: "120px",
+                boxShadow: "0 13px 43px 0 rgba(37, 46, 89, 0.1)"
               }}
             >
-              CHECK-IN
-              <br />
-              <input
-                type="date"
-                className="date"
-                style={{ backgroundColor: "white" }}
-              ></input>
-            </Col>
-            <Col
-              lg={3}
-              xs={12}
-              md={6}
-              style={{
-                backgroundColor: "white",
-                color: "black",
-                marginTop: "20px"
-              }}
-            >
-              CHECK-OUT
-              <br />
-              <input
-                type="date"
-                className="date"
-                style={{ backgroundColor: "white" }}
-              ></input>
-            </Col>
-            <Col
-              lg={3}
-              xs={12}
-              md={6}
-              style={{
-                marginTop: "0",
-                backgroundColor: "white",
-                color: "black"
-              }}
-            >
-              <br />
-              GUEST
-              <br />
-              <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Control
-                  as="select"
+              <Col
+                lg={3}
+                xs={12}
+                md={6}
+                style={{
+                  backgroundColor: "white",
+                  marginTop: "20px",
+                  color: "black"
+                }}
+              >
+                CHECK-IN
+                <br />
+                <input
+                  type="date"
                   className="date"
                   style={{ backgroundColor: "white" }}
+                ></input>
+              </Col>
+              <Col
+                lg={3}
+                xs={12}
+                md={6}
+                style={{
+                  backgroundColor: "white",
+                  color: "black",
+                  marginTop: "20px"
+                }}
+              >
+                CHECK-OUT
+                <br />
+                <input
+                  type="date"
+                  className="date"
+                  style={{ backgroundColor: "white" }}
+                ></input>
+              </Col>
+              <Col
+                lg={3}
+                xs={12}
+                md={6}
+                style={{
+                  marginTop: "0",
+                  backgroundColor: "white",
+                  color: "black"
+                }}
+              >
+                <br />
+                GUEST
+                <br />
+                <Form.Group controlId="exampleForm.ControlSelect1">
+                  <Form.Control
+                    as="select"
+                    className="date"
+                    style={{ backgroundColor: "white" }}
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col
+                lg={3}
+                xs={12}
+                md={6}
+                style={{ backgroundColor: "#C99635" }}
+                className="booknow"
+              >
+                <p
+                  className="booknow1"
+                  style={{
+                    // backgroundColor: "#C99635",
+                    textTransform: "uppercase",
+                    textAlign: "center",
+                    marginTop: "45px",
+                    color: "#ffffff",
+                    fontFamily: "Monserrat",
+                    fontWeight: "700",
+                    fontSize: "24px"
+                  }}
                 >
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Form.Control>
-              </Form.Group>
-            </Col>
-            <Col
-              lg={3}
-              xs={12}
-              md={6}
-              style={{ backgroundColor: "#C99635" }}
-              className="booknow"
-            >
-              <p
-                className="booknow1"
-                style={{
-                  // backgroundColor: "#C99635",
-                  textTransform: "uppercase",
-                  textAlign: "center",
-                  marginTop: "45px",
-                  color: "#ffffff",
-                  fontFamily: "Monserrat",
-                  fontWeight: "700",
-                  fontSize: "24px"
-                }}
-              >
-                book now
-              </p>
-            </Col>
-          </Row>
-        </Container>
+                  book now
+                </p>
+              </Col>
+            </Row>
+          </Container>
 
-        <Container style={{ marginTop: "150px" }} className="listrom">
-          <Row>
-            <Col className="text-center">
-              <p
-                style={{
-                  fontSize: "19px",
-                  fontFamily: "monserrat",
-                  color: "grey",
-                  lineHeight: "1px"
-                }}
-              >
-                CHOOSE FOR YOURSELF
-              </p>
-              <p
-                style={{
-                  fontSize: "50px",
-                  color: "#cb9734",
-                  fontFamily: "playfair display",
-                  marginBottom: "30px"
-                }}
-              >
-                Best Room
-              </p>
-              <img src={jcover2} alt="wlwl"></img>
-            </Col>
-          </Row>
-
-          {roomCards}
-        </Container>
-      </div>
-    </Fragment>
-  );
+          <Container style={{ marginTop: "150px" }} className="listrom">
+            <Row>
+              <Col className="text-center">
+                <p
+                  style={{
+                    fontSize: "19px",
+                    fontFamily: "monserrat",
+                    color: "grey",
+                    lineHeight: "1px"
+                  }}
+                >
+                  CHOOSE FOR YOURSELF
+                </p>
+                <p
+                  style={{
+                    fontSize: "50px",
+                    color: "#cb9734",
+                    fontFamily: "playfair display",
+                    marginBottom: "30px"
+                  }}
+                >
+                  Best Room
+                </p>
+                <img src={jcover2} alt="wlwl"></img>
+              </Col>
+            </Row>
+          </Container>
+          {this.state.data.map(data => {
+            return <RoomCard capacity={data.title} />;
+          })}
+        </div>
+      </Fragment>
+    );
+  }
 }
