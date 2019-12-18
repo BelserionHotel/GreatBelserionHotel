2008a987aaa8c5643a6a84dc055b55f18c8f058f
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-18 15:24:18 +0700

    Refactor Header in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,20 @@
 import React from "react";
-import { Jumbotron, Container, Row, Col, Button, Form } from "react-bootstrap";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import "../App.css";
-import jcover1 from "../assets/jcover1.png";
+
 import jcover2 from "../assets/jcover2.png";
 import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
+import { HeaderHome } from "../component";
+
 export default function Home() {
   return (
     <div>
-      <Jumbotron fluid className="jumbotron">
-        <Container>
-          <br />
-          <br />
-
-          <img
-            style={{
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto",
-              maxWidth: "100%",
-              height: "auto"
-            }}
-            src={jcover1}
-            alt="cover jumbotron 1"
-          />
-          <br />
-          <br />
-          <p
-            className="text-center jtext"
-            style={{
-              color: "white",
-              fontSize: "100px",
-              fontFamily: "Playfair Display",
-              lineHeight: "90px",
-              verticalAlign: "baseline",
-              letterSpacing: "5px",
-              wordSpacing: "0",
-              fontWeight: "700"
-            }}
-          >
-            Belserion Hotel
-          </p>
-
-          <h1
-            className="text-center"
-            style={{
-              color: "white",
-              fontSize: "50px"
-            }}
-          >
-            Luxury Resort & Spa
-          </h1>
-          <br />
-          <br />
-          <img
-            style={{
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto"
-            }}
-            src={jcover2}
-            alt="cover jumbotron 1"
-          />
-        </Container>
-      </Jumbotron>
+      <HeaderHome title="Belserion Hotel" subTitle="Luxury Resort &amp; Spa" />
 
       <Container>
         <Row className="cont">
           <Col xs="12" lg="3">
-            {/* <div className="makehome">Make Reservation</div> */}
             <div className="cinhome">
               Check-In
               <br />
@@ -76,7 +22,6 @@ export default function Home() {
             </div>
           </Col>
           <Col xs="12" lg="3">
-            {/* <div className="makehome">Make Reservation</div> */}
             <div className="couthome">
               Check-Out
               <br />
@@ -84,7 +29,6 @@ export default function Home() {
             </div>
           </Col>
           <Col xs="12" lg="3">
-            {/* <div className="makehome">Make Reservation</div> */}
             <div className="guesthome">
               Guest
               <br />
