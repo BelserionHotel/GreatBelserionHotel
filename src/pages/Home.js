5f78b3a5f94766500d9f76f8fda1a68590d8d124
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-18 09:10:41 +0700

    Merge the Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,116 @@
-import React, { Fragment } from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import React from "react";
+import { Jumbotron, Container, Row, Col, Button, Form } from "react-bootstrap";
 import "../App.css";
+import jcover1 from "../assets/jcover1.png";
 import jcover2 from "../assets/jcover2.png";
 import img1 from "../assets/img1.jpg";
-import img3 from "../assets/img3.jpg";
-
-import { HeaderHome, DatePanelBlack } from "../component";
-
+import img2 from "../assets/img2.jpg";
 export default function Home() {
   return (
-    <Fragment>
-      <HeaderHome
-        jumbotronTitle="Belserion Hotel"
-        jumbotronSubtitle="Luxury Resort &amp; Spa"
-      >
-        <DatePanelBlack />
-      </HeaderHome>
+    <div>
+      <Jumbotron fluid className="jumbotron">
+        <Container>
+          <br />
+          <br />
+
+          <img
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+              maxWidth: "100%",
+              height: "auto"
+            }}
+            src={jcover1}
+            alt="cover jumbotron 1"
+          />
+          <br />
+          <br />
+          <p
+            className="text-center jtext"
+            style={{
+              color: "white",
+              fontSize: "100px",
+              fontFamily: "Playfair Display",
+              lineHeight: "90px",
+              verticalAlign: "baseline",
+              letterSpacing: "5px",
+              wordSpacing: "0",
+              fontWeight: "700"
+            }}
+          >
+            Belserion Hotel
+          </p>
+
+          <h1
+            className="text-center"
+            style={{
+              color: "white",
+              fontSize: "50px"
+            }}
+          >
+            Luxury Resort & Spa
+          </h1>
+          <br />
+          <br />
+          <img
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+            src={jcover2}
+            alt="cover jumbotron 1"
+          />
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <Row className="cont">
+          <Col xs="12" lg="3">
+            {/* <div className="makehome">Make Reservation</div> */}
+            <div className="cinhome">
+              Check-In
+              <br />
+              <input type="date" className="date1"></input>
+            </div>
+          </Col>
+          <Col xs="12" lg="3">
+            {/* <div className="makehome">Make Reservation</div> */}
+            <div className="couthome">
+              Check-Out
+              <br />
+              <input type="date" className="date1"></input>
+            </div>
+          </Col>
+          <Col xs="12" lg="3">
+            {/* <div className="makehome">Make Reservation</div> */}
+            <div className="guesthome">
+              Guest
+              <br />
+              <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Control as="select" className="date1">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Form.Control>
+              </Form.Group>
+            </div>
+          </Col>
+          <Col xs="12" lg="3">
+            <div className="makehome">
+              {" "}
+              <br style={{ lineHeight: "40px" }} />
+              Make A Reservation
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
       <div className="text-center wrap" style={{ marginTop: "238px" }}>
-        <Row style={{ width: "100%", margin: "auto" }}>
+        <Row>
           <Col xl={8} lg={12} xs={12}>
             <Row>
               <Col xs={12} lg={6} xl={6} className="image1">
@@ -71,56 +163,11 @@ export default function Home() {
               </Col>
             </Row>
           </Col>
-          <Col xl={4} lg={12} xs={12} className="image2">
-            <img src={img3} alt="wkwkw" className="img12" />
-          </Col>
-        </Row>
-        <Row className="bawah" style={{ width: "100%", margin: "auto" }}>
-          <Col
-            xs={12}
-            lg={12}
-            xl={12}
-            style={{
-              backgroundColor: "black",
-              color: " #e2c285",
-              width: "100%",
-              border: "1px solid #e2c285"
-            }}
-          >
-            <p
-              style={{
-                marginTop: "50px",
-                fontFamily: "Playfair Display",
-                fontSize: "35px"
-              }}
-            >
-              Best Experience
-            </p>
-            <img src={jcover2} alt="wkwk" />
-            <p style={{ marginTop: "30px" }}>
-              <span
-                style={{
-                  color: "#blaca3",
-                  position: "relative",
-                  display: "inline-block",
-                  width: "70%"
-                }}
-              >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-                praesentium nobis ratione molestias eligendi repellat qui eius
-                cumque ipsam maxime voluptate pariatur ducimus, iure iusto ipsa,
-                dolores at quas quis?
-              </span>
-            </p>
-            <Button
-              style={{ marginTop: "30px", marginBottom: "50px" }}
-              variant="outline-light"
-            >
-              learn more
-            </Button>
+          <Col xl={4} lg={12} xs={12} className="image1">
+            <img src={img2} alt="wkwkw" className="img12 imm" />
           </Col>
         </Row>
       </div>
-    </Fragment>
+    </div>
   );
 }
