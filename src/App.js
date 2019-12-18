5d8eadd2a432b4b9ee35b21e118f06f4c2e3b129
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-18 15:23:52 +0700

    Delete Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
